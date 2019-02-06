160e635b67cfc977638c4823128e84876b26be13
Ko Takagi <knockoutmarch@gmail.com>
2019-02-06 14:46:38 +0900

    Fixed export shell

--- bin/export.sh
+++ bin/export.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker-compose run --rm db bash -c 'exec mysqldump -h db -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE"' > wordpress.sql
+DUMP_FILE=wordpress.sql
+
+docker-compose run --rm db bash -c 'exec mysqldump -h db -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE"' > $DUMP_FILE
+
+HEAD=`head -1 $DUMP_FILE`
+if [[ $HEAD =~ Warning ]] ;
+then
+	sed -i -e '1,1d' $DUMP_FILE
+fi
